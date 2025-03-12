57af69ae8c96e722a55602042deecb8da2ff2410
morgan.philippe <morgan.philippe@universite-paris-saclay.fr>
2025-03-12 15:56:06 +0100

    Petites Modifs

---
+++ src/main/java/com/et4/gametrackerproject/controller/api/AchievementApi.java
@@ -0,0 +1,6 @@
+package com.et4.gametrackerproject.controller.api;
+
+import static com.et4.gametrackerproject.utils.Constants.APP_ROOT;
+
+public interface AchievementApi {
+}
--- src/main/java/com/et4/gametrackerproject/utils/JsonConverter.java
+++ src/main/java/com/et4/gametrackerproject/utils/JsonConverter.java
@@ -1,4 +1,4 @@
-package com.et4.gametrackerproject.util;
+package com.et4.gametrackerproject.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
