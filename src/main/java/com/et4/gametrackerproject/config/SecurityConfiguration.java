9117f4e3fe90bd3dd779c3d563f29b6d15bfe5ff
morgan.philippe <morgan.philippe@universite-paris-saclay.fr>
2025-03-26 17:26:31 +0100

    Starting of authentication service implementation with user details and security configuration

---
+++ src/main/java/com/et4/gametrackerproject/config/SecurityConfiguration.java
@@ -0,0 +1,47 @@
+package com.et4.gametrackerproject.config;
+
+import com.et4.gametrackerproject.services.auth.ApplicationUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Autowired
+    private ApplicationUserDetailsService applicationUserDetailsService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/**/authenticate").permitAll()
+                .anyRequest().authenticated()
+            );
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(applicationUserDetailsService);
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+}
---
+++ src/main/java/com/et4/gametrackerproject/dto/auth/AuthenticationRequest.java
@@ -0,0 +1,14 @@
+package com.et4.gametrackerproject.dto.auth;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class AuthenticationRequest {
+
+    private String login;
+
+    private String password;
+
+}
