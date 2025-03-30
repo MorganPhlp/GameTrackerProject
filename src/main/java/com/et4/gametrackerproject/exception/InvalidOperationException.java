4b6af2ed0439e0f80132b9364bd89291111ef58e
morgan.philippe <morgan.philippe@universite-paris-saclay.fr>
2025-03-31 00:39:23 +0200

    Add photo management functionality with strategy pattern and error handling

---
+++ src/main/java/com/et4/gametrackerproject/exception/InvalidOperationException.java
@@ -0,0 +1,24 @@
+package com.et4.gametrackerproject.exception;
+
+public class InvalidOperationException extends RuntimeException {
+
+    private ErrorCodes errorCode;
+
+    public InvalidOperationException(String message) {
+        super(message);
+    }
+
+    public InvalidOperationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InvalidOperationException(String message, Throwable cause, ErrorCodes errorCode) {
+        super(message, cause);
+        this.errorCode = errorCode;
+    }
+
+    public InvalidOperationException(String message, ErrorCodes errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+}
